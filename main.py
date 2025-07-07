5af7f4181aeb02debefe8c105a24801df0579b4f
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-07 16:16:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,12 @@ def index():
     return render_template("not_logged_in.html")
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+@app.route('login')
+def login():
+  pass
+
+@app.route('login-callback')
+def logincallback():
+  code = request.args.get("code")
+  return "."
