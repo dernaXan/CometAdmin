486ce77149938cd5e92fd33ea9caf4803a7a9cb3
Tarani-2020 <97033898+Tarani-2020@users.noreply.github.com>
2025-07-07 14:37:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import flask
+from flask import request
+from flask import Flask, make_response
+import secrets
+
+def set_cookie(cookie, lifetime=604800, secure=False):
+  for key, value in cookies.items():
+    try:
+      resp.set_cookie(key, value)
+    except:
+      resp = make_response(f"Cookies: {cookie}")
+      resp.set_cookie(key, value, secure=secure, httponly=secure)
+
+def get_cookie(key):
+  data = request.cookies.get(key)
+  return data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  if get_cookie('session'):
+    return 'Hallo'
+  else:
+    
